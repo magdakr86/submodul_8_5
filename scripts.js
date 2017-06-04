0ad7181b8f4e50fb8c4ea2f042210b4257b8698e
Magdalena Michalska <magda@MacBook-Pro-Magdalena.local>
2017-06-04 22:45:23 +0200

    add scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 // scripts.js
 
-var a = prompt('Enter the base a of a Triangle')
-    h = prompt('Enter the height h of a Triangle')
+var a = prompt('Enter the base a of a Triangle'),
+    h = prompt('Enter the height h of a Triangle'),
     triangleArea = a*h/2;
 alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
 console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
